7a5fa28a82b48b533e9cee2fd28b7bf3b181296e
atheesa <nantheesanr@gmail.com>
2026-01-12 15:14:33 +0000

    add more pointer qs

---
+++ twoPointer/twoSumSorted.py
@@ -0,0 +1,15 @@
+def two_sum_sorted(arr: list[int], target: int) -> list[int]:
+    l,r = 0,0
+    
+    
+    
+    return []
+
+
+
+
+if __name__ == "__main__":
+    arr = [int(x) for x in input().split()]
+    target = int(input())
+    res = two_sum_sorted(arr, target)
+    print(" ".join(map(str, res)))
